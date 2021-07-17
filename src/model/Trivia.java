9d8017d7d4b1fdfa74956cc0e40fc5487ed4c533
Raz Consta <66283215+RazConsta@users.noreply.github.com>
2021-07-17 16:32:33 -0700

    Database design 1.0

---
+++ src/model/Trivia.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Trivia {
+
+    private final String myQuestion;
+    private final String myAnswer;
+
+    public Trivia(String theQuestion, String theAnswer)
+    {
+        myQuestion = theQuestion;
+        myAnswer = theAnswer;
+    }
+
+    public Trivia getTrivia()
+    {
+        return null;
+    }
+
+    public Trivia getAnswer(Trivia myTrivia)
+    {
+        return null;
+    }
+
+}

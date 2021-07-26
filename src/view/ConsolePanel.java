956eae84b5fbc6324aac7777e76471e34f52acf6
Dustin Ray <dustiv2@uw.edu>
2021-07-25 20:19:58 -0700

    fires property change when in proximity to a given door

--- src/view/ConsolePanel.java
+++ src/view/ConsolePanel.java
@@ -17,57 +17,56 @@ import static controller.PropertyChangeEnabledUserControls.PROPERTY_PROXIMITY_DO
 public class ConsolePanel extends JPanel implements PropertyChangeListener {
 
 
-    final JTextArea consoleScreen;
+    JTextArea consoleScreenTextArea;
 
-    BufferedImage myBackground1;
+    BufferedImage displayConsole;
+    BufferedImage infoConsole;
     public ConsolePanel() throws IOException, FontFormatException {
-
-        myBackground1 = ImageIO.read(new File("src/res/backgrounds/console.png"));
+        displayConsole = ImageIO.read(new File("src/res/assets/console.png"));
+        infoConsole = ImageIO.read(new File("src/res/assets/info_console.png"));
         this.setLayout(null);
-        this.repaint();
-        Font fontTest = Font.createFont(Font.TRUETYPE_FONT, new File("src/res/fonts/expansiva/Expansiva.otf"));
-        fontTest = fontTest.deriveFont(Font.PLAIN, 14);
-        consoleScreen = new JTextArea("this is a test...");
-        consoleScreen.setVisible(true);
-        consoleScreen.setForeground(Color.WHITE);
-        consoleScreen.setBackground(Color.BLACK);
-        consoleScreen.setBounds(830, 50, 360, 245);
-        consoleScreen.setLayout(null);
-        consoleScreen.setFont(fontTest);
-        this.add(consoleScreen);
+        setupText();
     }
 
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D mainBackground = (Graphics2D) g;
-        mainBackground.drawImage(myBackground1, 768, 0, null);
-
+        Graphics2D displayConsole = (Graphics2D) g;
+        displayConsole.drawImage(this.displayConsole, 768, 0, null);
+        displayConsole.drawImage(this.infoConsole, 768, 480, null);
     }
-
 
     @Override
     public void propertyChange(final PropertyChangeEvent theEvent) {
         switch (theEvent.getPropertyName()) {
             case PROPERTY_PROXIMITY_DOOR_A -> {
-                consoleScreen.setText("Property change fired: " + "\n" + "next to door A");
+                consoleScreenTextArea.setText("Property change fired: " + "\n" + "next to door A");
             }
             case PROPERTY_PROXIMITY_DOOR_B -> {
-                consoleScreen.setText("Property change fired: " + "\n" + "next to door B");
+                consoleScreenTextArea.setText("Property change fired: " + "\n" + "next to door B");
             }
             case PROPERTY_PROXIMITY_DOOR_C -> {
-                consoleScreen.setText("Property change fired: " + "\n" + "next to door C");
+                consoleScreenTextArea.setText("Property change fired: " + "\n" + "next to door C");
             }
             case PROPERTY_PROXIMITY_DOOR_D -> {
-                consoleScreen.setText("Property change fired: " + "\n" + "next to door D");
+                consoleScreenTextArea.setText("Property change fired: " + "\n" + "next to door D");
             }
         }
     }
 
     public void setupText() throws IOException, FontFormatException {
 
-
+        Font fontTest = Font.createFont(Font.TRUETYPE_FONT, new File("src/res/fonts/expansiva/Expansiva.otf"));
+        fontTest = fontTest.deriveFont(Font.PLAIN, 18);
+        consoleScreenTextArea = new JTextArea("this is a test...");
+        consoleScreenTextArea.setVisible(true);
+        consoleScreenTextArea.setForeground(Color.WHITE);
+        consoleScreenTextArea.setBackground(Color.BLACK);
+        consoleScreenTextArea.setBounds(830, 50, 360, 245);
+        consoleScreenTextArea.setLayout(null);
+        consoleScreenTextArea.setFont(fontTest);
+        this.add(consoleScreenTextArea);
 
     }
 

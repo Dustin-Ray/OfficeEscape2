6468275884972b89495b585c5cc4980472664e4f
Dustin Ray <dustiv2@uw.edu>
2021-07-29 06:04:21 -0700

    restore room traversal functionality with new map layout

---
+++ src/res/floor_maps/floor_map_2/modern_office_walls_floors_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="modern_office_walls_floors_48x48" tilewidth="48" tileheight="48" tilecount="224" columns="16">
+ <image source="../../icons/tilesets/modern_office_walls_floors_48x48.png" width="768" height="672"/>
+</tileset>

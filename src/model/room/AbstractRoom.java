1cb84dabcf35fdcef1312d2ff23649225c895238
Reuben Keller <reubenkeller@protonmail.com>
2021-08-11 23:51:21 -0700

    Cleaned up documentation and removed unnecessary methods.

--- src/model/room/AbstractRoom.java
+++ src/model/room/AbstractRoom.java
@@ -11,101 +11,111 @@ Team members: Dustin Ray, Raz Consta, Reuben Keller
 package model.room;
 
 import model.map.GameMap;
-import model.map.Terrain;
 
 /**
- * Implements behavior common to all Rooms.
+ * Implements behavior common to all Rooms. Each Room has 4 possible Doors and
+ * 4 possible neighboring Rooms.
  *
  * @author Reuben Keller
+ * @version Summer 2021
  */
 public abstract class AbstractRoom {
 
-    /** Door object containing a given trivia question and link to another room. */
-    private Door DoorA;
-    /** Door object containing a given trivia question and link to another room. */
-    private Door DoorB;
-    /** Door object containing a given trivia question and link to another room. */
-    private Door DoorC;
-    /** Door object containing a given trivia question and link to another room. */
-    private Door DoorD;
+    /** Door A of this Room. */
+    private Door doorA;
 
-    /** Room object possibly linked to this room by a given door. */
-    private Room RoomA;
-    /** Room object possibly linked to this room by a given door. */
-    private Room RoomB;
-    /** Room object possibly linked to this room by a given door. */
-    private Room RoomC;
-    /** Room object possibly linked to this room by a given door. */
-    private Room RoomD;
+    /** Door B of this Room. */
+    private Door doorB;
 
-    /** The int ID for this room. */
+    /** Door C of this Room. */
+    private Door doorC;
+
+    /** Door D of this Room. */
+    private Door doorD;
+
+    /** The Room linked to this Room by Door A. */
+    private Room roomA;
+
+    /** The Room linked to this Room by Door B. */
+    private Room roomB;
+
+    /** The Room linked to this Room by Door C. */
+    private Room roomC;
+
+    /** The Room linked to this Room by Door D. */
+    private Room roomD;
+
+    /** The unique integer ID of this Room. */
     private final int myID;
-    /** The terrain grid for this room. */
-    private final Terrain[][] myTerrain;
 
+    /** The GameMap of this Room. */
     private final GameMap gm;
 
 
     /**
      * Constructs a Room with the given integer ID.
+     *
      * @param theID The integer ID of this Room.
      */
     public AbstractRoom(final int theID) {
         myID = theID;
-        GameMap tG = new GameMap(theID);
-        myTerrain = tG.getTerrainGrid();
         gm = new GameMap(myID);
     }
 
+
+    /**
+     * Returns the GameMap for this Room.
+     *
+     * @return The GameMap for this Room.
+     */
     public GameMap getMap() {
         return gm;
     }
 
-    /**
-     * Gets the terrain grid for this room.
-     * @return 2D character array representing terrain grid for this room.
-     */
-    public Terrain[][] getTerrain() {
-        return myTerrain;
-    }
 
     /**
      * Returns the integer ID of for this Room.
+     *
      * @return The integer ID for this Room.
      */
     public int getRoomID() {
         return myID;
     }
 
+
     /**
-     * Gets the door specified if it exists.
-     * @param theDoorName A letter representing the door to return.
-     * @return the door specified by theDoorName.
+     * Returns the Door specified by the given letter ID.
+     *
+     * @param letterID The letter representing the Door to return.
+     * @return The Door specified by letterID.
      */
-    public Door getDoor(final String theDoorName) {
-        return switch (theDoorName) {
-            case "A" -> DoorA;
-            case "B" -> DoorB;
-            case "C" -> DoorC;
-            case "D" -> DoorD;
+    public Door getDoor(final String letterID) {
+        return switch (letterID) {
+            case "A" -> doorA;
+            case "B" -> doorB;
+            case "C" -> doorC;
+            case "D" -> doorD;
             default -> null;
         };
     }
 
+
     /**
-     * Gets the room specified if it exists.
-     * @param theRoomName A letter representing the room to return.
-     * @return the room specified by theRoomName.
+     * Returns the room specified by the given letter ID.
+     *
+     * @param letterID A letter representing the Room to return.
+     * @return the Room specified by letterID.
      */
-    public Room getRoom(final String theRoomName) {
-        return switch (theRoomName) {
-            case "A" -> RoomA;
-            case "B" -> RoomB;
-            case "C" -> RoomC;
-            case "D" -> RoomD;
+    public Room getRoom(final String letterID) {
+        return switch (letterID) {
+            case "A" -> roomA;
+            case "B" -> roomB;
+            case "C" -> roomC;
+            case "D" -> roomD;
             default -> null;
         };
     }
+
 
     /**
      * Checks to see if the specified room exists.
@@ -115,158 +125,60 @@ public abstract class AbstractRoom {
      */
     public boolean hasRoom(final String theRoomName) {
         return switch (theRoomName) {
-            case "A" -> RoomA != null;
-            case "B" -> RoomB != null;
-            case "C" -> RoomC != null;
-            case "D" -> RoomD != null;
+            case "A" -> roomA != null;
+            case "B" -> roomB != null;
+            case "C" -> roomC != null;
+            case "D" -> roomD != null;
             default -> false;
         };
     }
 
 
     /**
-     * Sets the A Room and Door to the given Room and Door.
+     * Sets Room A and Door A to the given Room and Door.
      *
-     * @param room The A Room of this Room.
-     * @param door The A Door separating this Room and the A Room.
+     * @param room Room A of this Room.
+     * @param door Door A separating this Room and Room A.
      */
     public void setA(final Room room, final Door door) {
-        RoomA = room;
-        DoorA = door;
+        roomA = room;
+        doorA = door;
     }
 
 
     /**
-     * Sets the B Room and Door to the given Room and Door.
+     * Sets Room B and Door B to the given Room and Door.
      *
-     * @param room The B Room of this Room.
-     * @param door The B Door separating this Room and the B Room.
+     * @param room Room B of this Room.
+     * @param door Door B separating this Room and the Room B.
      */
     public void setB(final Room room, final Door door) {
-        RoomB = room;
-        DoorB = door;
+        roomB = room;
+        doorB = door;
     }
 
 
     /**
-     * Sets the C Room and Door to the given Room and Door.
+     * Sets Room C and Door C to the given Room and Door.
      *
-     * @param room The C Room of this Room.
-     * @param door The C Door separating this Room and the C Room.
+     * @param room Room C of this Room.
+     * @param door Door C separating this Room and the Room C.
      */
     public void setC(final Room room, final Door door) {
-        RoomC = room;
-        DoorC = door;
+        roomC = room;
+        doorC = door;
     }
 
 
     /**
-     * Sets the D Room and Door to the given Room and Door.
+     * Sets Room D and Door D to the given Room and Door.
      *
-     * @param room The D Room of this Room.
-     * @param door The D Door separating this Room and the D Room.
+     * @param room Room D of this Room.
+     * @param door Door D separating this Room and Room D.
      */
     public void setD(final Room room, final Door door) {
-        RoomD = room;
-        DoorD = door;
-    }
-
-
-    /**
-     * Returns the door between this Room and the north Room.
-     *
-     * @return The Door between this Room and the north Room.
-     * @throws NullPointerException if this Room does not have a north Room.
-     */
-    public Door getDoorA() {
-        if (!hasRoomA()) {
-            throw new NullPointerException("Room is null (i.e., not connected)");
-        }
-        return DoorA;
-    }
-
-
-    /**
-     * Returns the door between this Room and the south Room.
-     *
-     * @return The Door between this Room and the south Room.
-     * @throws NullPointerException if this Room does not have a south Room.
-     */
-    public Door getDoorB() {
-        if (!hasRoomB()) {
-            throw new NullPointerException("Room B is null (i.e., not connected)");
-        }
-        return DoorB;
-    }
-
-
-    /**
-     * Returns the door between this Room and the east Room.
-     * @return The Door between this Room and the east Room.
-     * @throws NullPointerException if this Room does not have an east Room.
-     */
-    public Door getDoorC() {
-        if (!hasRoomC()) {
-            throw new NullPointerException("Room C is null (i.e., not connected)");
-        }
-        return DoorC;
-    }
-
-
-    /**
-     * Returns the door between this Room and the west Room.
-     * @return The Door between this Room and the west Room.
-     * @throws NullPointerException if this Room does not have a west Room.
-     */
-    public Door getDoorD() {
-        if (!hasRoomD()) {
-            throw new NullPointerException("Room D is null (i.e., not connected)");
-        }
-        return DoorD;
-    }
-
-
-    /**
-     * Returns true if this Room is connected to a north Room and false
-     * otherwise.
-     * @return true if this Room is connected to a north Room and false
-     *     otherwise.
-     */
-    public boolean hasRoomA() {
-        return RoomA != null;
-    }
-
-
-
-
-    /**
-     * Returns true if this Room is connected to a south Room and false
-     * otherwise.
-     * @return true if this Room is connected to a south Room and false
-     *     otherwise.
-     */
-    public boolean hasRoomB() {
-        return RoomB != null;
-    }
-
-    /**
-     * Returns true if this Room is connected to a east Room and false
-     * otherwise.
-     * @return true if this Room is connected to a east Room and false
-     *     otherwise.
-     */
-    public boolean hasRoomC() {
-        return RoomC != null;
-    }
-
-    /**
-     * Returns true if this Room is connected to a west Room and false
-     * otherwise.
-     * @return true if this Room is connected to a west Room and false
-     *     otherwise.
-     */
-    public boolean hasRoomD() {
-        return RoomD != null;
+        roomD = room;
+        doorD = door;
     }
 
 }

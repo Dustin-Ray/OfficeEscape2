f111e0b08a2bd91a2af9512e77642a240be529b8
Dustin Ray <dustiv2@uw.edu>
2021-08-18 11:16:37 -0700

    refactor

--- src/view/AbstractInfoPanel.java
+++ src/view/AbstractInfoPanel.java
@@ -6,6 +6,11 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
+/**
+ * Abstract panel used to display text and images containing information about the game.
+ * @author Dustin Ray
+ * @version Summer 2021
+ */
 public abstract class AbstractInfoPanel extends JPanel {
 
     /**background image for panel.  */
@@ -23,12 +28,20 @@ public abstract class AbstractInfoPanel extends JPanel {
     public AbstractInfoPanel(final int theWidth, final int theHeight, final String thePath) {
 
         myPath = thePath;
+        setupPanel(theWidth, theHeight);
+        readFiles();
+        repaint();
+    }
+
+    /**
+     * Initializes the panel.
+     * @param theWidth width of panel.
+     * @param theHeight height of panel.
+     */
+    private void setupPanel(int theWidth, int theHeight) {
         setPreferredSize(new Dimension(theWidth, theHeight));
         setBackground(Color.BLACK);
         setVisible(true);
-        readFiles();
-
-        repaint();
     }
 
 

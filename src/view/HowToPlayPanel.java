e63c9cd8f23f4491ff049c93c047c7ce937e2b79
Dustin Ray <dustiv2@uw.edu>
2021-08-18 00:32:07 -0700

    refactor

--- src/view/HowToPlayPanel.java
+++ src/view/HowToPlayPanel.java
@@ -43,9 +43,8 @@ public class HowToPlayPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(image, 0,0, null);
-        repaint();
+        g.drawImage(image, 0,0, null);
+
     }
 
 }
